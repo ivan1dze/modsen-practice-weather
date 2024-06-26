f4348b9ec9742e38a1bea85d40cba3a7819b364c
quickjeembo <vanya9782@gmail.com>
2024-06-26 12:28:41 +0300

    feat: added ESLint and Prettier rules configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:prettier/recommended'
+    'prettier'
   ],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error'
+  },
   settings: {
     react: {
       version: 'detect'
     }
-  },
-  rules: {
-    'react/react-in-jsx-scope': 'off'
   }
 };
